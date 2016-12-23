a1947b483e145c88be93fa98e7b373940a94f5f0
Isaac Halvorson <hello@hisaac.net>
2016-12-23 11:36:47 -0500

    initialize repo

---
+++ script.js
@@ -0,0 +1,17 @@
+//----------------------------------------------------------------------------//
+
+console.log('getelements', document.getElementsByTagName('button'));
+
+// function search(pressedButton){
+// 	// var searchProvider = pressedButton;
+// 	var query = document.getElementById('query').value;
+// 	console.log('button pressed:', pressedButton.outerHTML);
+// 	console.log('query:', query);	
+// }
+
+//----------------------------------------------------------------------------//
+
+/*
+	# resources
+	- http://blog.garstasio.com/you-dont-need-jquery/
+*/
